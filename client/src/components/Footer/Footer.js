bcd4f3f8986270a9b0347b00fe9610977cdc392b
sophchoe <schoe@pixameter.com>
2024-02-02 17:24:30 -0800

    Create Footer.js

---
+++ client/src/components/Footer/Footer.js
@@ -0,0 +1,17 @@
+// client/src/components/Footer/Footer.js
+
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="footer-content">
+        {/* Your footer content goes here */}
+        <p>&copy; 2024 Your App. All rights reserved.</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
