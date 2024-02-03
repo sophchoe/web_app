33929b1c27818d0ae7a9a77e9cb9f68b2a13089a
sophchoe <schoe@pixameter.com>
2024-02-02 17:32:10 -0800

    Create FeaturePosts.js

---
+++ client/src/components/HomePage/FeaturePosts/FeaturePosts.js
@@ -0,0 +1,14 @@
+// client/src/components/HomePage/FeaturedPosts/FeaturedPosts.js
+
+import React from 'react';
+
+const FeaturedPosts = () => {
+  // Logic to fetch or display featured posts
+  return (
+    <div className="featured-posts">
+      {/* Featured posts content goes here */}
+    </div>
+  );
+};
+
+export default FeaturedPosts;
