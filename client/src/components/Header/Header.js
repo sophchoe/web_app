44361db69e90c80a5fd62956dc412803e8b1bd97
sophchoe <schoe@pixameter.com>
2024-02-02 17:23:24 -0800

    Create Header.js

---
+++ client/src/components/Header/Header.js
@@ -0,0 +1,14 @@
+// client/src/components/Header/Header.js
+
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+  return (
+    <header className="header">
+      {/* Your header content goes here */}
+    </header>
+  );
+};
+
+export default Header;
