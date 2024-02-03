459f43bb84d378d20db021d474ae6ff0f3852431
sophchoe <schoe@pixameter.com>
2024-02-02 17:33:06 -0800

    Create RecentPosts.js

---
+++ client/src/components/HomePage/RecentPosts/RecentPosts.js
@@ -0,0 +1,14 @@
+// client/src/components/HomePage/RecentPosts/RecentPosts.js
+
+import React from 'react';
+
+const RecentPosts = () => {
+  // Logic to fetch or display recent posts
+  return (
+    <div className="recent-posts">
+      {/* Recent posts content goes here */}
+    </div>
+  );
+};
+
+export default RecentPosts;
