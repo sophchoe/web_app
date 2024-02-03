d85a1b008bbba4d1d8d7144f48d3361421eac6b6
sophchoe <schoe@pixameter.com>
2024-02-02 17:45:30 -0800

    Create index.js

---
+++ client/src/index.js
@@ -0,0 +1,13 @@
+// client/src/index.js
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './index.css'; // Optional: Import global styles
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
